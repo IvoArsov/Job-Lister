aeb93e9e4f9c70fc1eaac30d2c2e3ea05f6b3a59
IvoArsov <arsov.ivaylo@gmail.com>
2020-10-13 16:45:55 +0300

    First work commit - NOT FINISHED

---
+++ templates/inc/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job Lister</title>
+    <link rel="stylesheet" href="https://bootswatch.com/4/superhero/bootstrap.min.css"/>
+    <link rel="stylesheet" href="css/styles.css"/>
+</head>
+<body>
+HEADER
