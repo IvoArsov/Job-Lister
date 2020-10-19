3457ac3d75b924831f9453599db0481ec7a8b410
IvoArsov <arsov.ivaylo@gmail.com>
2020-10-19 17:49:38 +0300

    add some CSS

--- templates/frontpage.php
+++ templates/frontpage.php
@@ -1,33 +1,41 @@
-<?php include "inc/header.php"; ?>
-<div class="jumbotron">
-  <h1 class="display-4">Find a job</h1>
-  <form action="index.php" method="GET">
-    <select name="category" class="form-control">
-      <option value="0">Choose Category</option>
-      <?php foreach($categories as $category) : ?>
-        <option value="<?php echo $category->id; ?>"><?php echo $category->name; ?></option>
-      <?php endforeach; ?>
-    </select>
-    <br/>
-    <input type="submit" class="btn btn-primary" value="FIND" />      
-  </form>
-  
-</div>
+<div class="container">
+  <?php include "inc/header.php"; ?>
 
-<div class="col-md-6">
-  <h2><?php echo $title; ?></h2>
-  <br/>
-<?php foreach($jobs as $job): ?>
-  <div class="row">
-    <div class="col-md-10">
-      <h4><?php echo $job->job_title; ?></h4>
-      <p><?php echo $job->description; ?></p>
+
+  <div class="jumbotron">
+    <div class="col-md-3">&nbsp;</div>
+    <div class="col-md-6">
+      <h1 class="display-4">Find a job</h1>
+      <form action="index.php" method="GET">
+        <select name="category" class="form-control">
+          <option value="0">Choose Category</option>
+          <?php foreach ($categories as $category) : ?>
+            <option value="<?php echo $category->id; ?>"><?php echo $category->name; ?></option>
+          <?php endforeach; ?>
+        </select>
+        <br />
+        <input type="submit" class="btn btn-outline-success" value="FIND" />
+      </form>
     </div>
-    <div class="col-md-2">
-      <a href="#" class="btn btn-primary">View</a>
-    </div>
+    <div class="col-md-3">&nbsp;</div>
   </div>
-  <?php endforeach;?>
-</div>
 
-<?php include "inc/footer.php"; ?>
+
+  <div class="col-md-12">
+    <h2><?php echo $title; ?></h2>
+    <br />
+    <?php foreach ($jobs as $job) : ?>
+      <div class="row">
+        <div class="col-md-10">
+          <h4><?php echo $job->job_title; ?></h4>
+          <p><?php echo $job->description; ?></p>
+        </div>
+        <div class="col-md-2">
+          <a href="#" class="btn btn-outline-info">View</a>
+        </div>
+      </div>
+    <?php endforeach; ?>
+  </div>
+
+  <?php include "inc/footer.php"; ?>
+</div>
