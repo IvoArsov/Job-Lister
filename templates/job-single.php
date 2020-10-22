323504ad50e1c5cd356438d3ba8e8cecb5ca6fd7
IvoArsov <arsov.ivaylo@gmail.com>
2020-10-22 19:04:51 +0300

    Add Delete and Edit

--- templates/job-single.php
+++ templates/job-single.php
@@ -1,16 +1,25 @@
  <div class="container">
-  <?php include "inc/header.php"; ?>
-    <h2 class="page-header"><?php echo $job->job_title; ?> (<?php echo $job->location; ?>)</h2>
-    <small>Posted by: <?php echo $job->contact_user; ?> on <em><?php echo $job->post_date; ?></em></small>
-    <hr />
-    <p class="lead"><?php echo $job->description; ?></p>
-    <ul class="list-group">
-        <li class="list-group-item"><strong>Company: </strong><?php echo $job->company; ?></li>
-        <li class="list-group-item"><strong>Salary: </strong><?php echo $job->salary; ?></li>
-        <li class="list-group-item"><strong>Contact Email: </strong><?php echo $job->contact_email; ?></li>
-    </ul>
-    <br />
-    <br />
-    <a href="index.php" class="btn btn-primary">BACK</a>
-  <?php include "inc/footer.php"; ?>
-</div>
+   <?php include "inc/header.php"; ?>
+   <h2 class="page-header"><?php echo $job->job_title; ?> (<?php echo $job->location; ?>)</h2>
+   <small>Posted by: <?php echo $job->contact_user; ?> on <em><?php echo $job->post_date; ?></em></small>
+   <hr />
+   <p class="lead"><?php echo $job->description; ?></p>
+   <ul class="list-group">
+     <li class="list-group-item"><strong>Company: </strong><?php echo $job->company; ?></li>
+     <li class="list-group-item"><strong>Salary: </strong><?php echo $job->salary; ?></li>
+     <li class="list-group-item"><strong>Contact Email: </strong><?php echo $job->contact_email; ?></li>
+   </ul>
+   <br />
+   <br />
+   <a href="index.php" class="btn btn-primary">BACK</a>
+   <br />
+   <br />
+   <div class="well">
+     <a href="edit.php?id=<?php echo $job->id; ?>" class="btn btn-success">EDIT</a>
+     <form action="job.php" style="display:inline;" method="post">
+        <input type="hidden" name="del_id" value="<?php echo $job->id; ?>"/>
+        <input type="submit" class="btn btn-danger" value="DELETE" />
+     </form>
+   </div>
+   <?php include "inc/footer.php"; ?>
+ </div>
