6fe3a4f901bfdabc2b5d3866cbfe393e05fb6bcf
IvoArsov <arsov.ivaylo@gmail.com>
2020-10-23 11:39:32 +0300

    Small changes

--- templates/job-single.php
+++ templates/job-single.php
@@ -1,7 +1,7 @@
  <div class="container">
    <?php include "inc/header.php"; ?>
    <h2 class="page-header"><?php echo $job->job_title; ?> (<?php echo $job->location; ?>)</h2>
-   <small>Posted by: <?php echo $job->contact_user; ?> on <em><?php echo $job->post_date; ?></em></small>
+   <small>Posted by: <strong><?php echo $job->contact_user; ?></strong> on <em><?php echo $job->post_date; ?></em></small>
    <hr />
    <p class="lead"><?php echo $job->description; ?></p>
    <ul class="list-group">
